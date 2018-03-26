7c42b9ba8a8a3897d38074514ba855ec42326908
bfaulkn <bfaulkn@siue.edu>
2018-03-25 23:22:15 -0500

    added logic to hopefully allow all paths

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+app.get('/*all', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
