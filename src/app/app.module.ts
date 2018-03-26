85574d12501f47ba95e1e10413bbab2d62038057
bfaulkn <bfaulkn@siue.edu>
2018-03-25 22:59:50 -0500

    Created simple scorecard app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,68 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  { path: '',
+    component: HomeComponent
+  },
+  { path: 'new',
+    component: HomeComponent,
+    data: {action: 'reset'}
+  },
+  { path: 'red/:num',
+    component: HomeComponent,
+    data: {action: 'setRed'}
+  },
+  { path: 'blue/:num',
+    component: HomeComponent,
+    data: {action: 'setBlue'}
+  },
+  { path: 'increment-blue',
+    component: HomeComponent,
+    data: {action: 'incrementBlue'}
+  },
+  { path: 'increment-blue/:num',
+    component: HomeComponent,
+    data: {action: 'incrementBlue'}
+  },
+  { path: 'increment-red',
+    component: HomeComponent,
+    data: {action: 'incrementRed'}
+  },
+  { path: 'increment-red/:num',
+    component: HomeComponent,
+    data: {action: 'incrementRed'}
+  },
+  { path: 'reset',
+    component: HomeComponent,
+    data: {action: 'reset'}
+  },
+  { path: '**',
+    redirectTo: '/',
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
-  providers: [],
+  providers: [{ provide: 'LOCALSTORAGE', useFactory: getLocalStorage }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function getLocalStorage() {
+  return (typeof window !== "undefined") ? window.localStorage : null;
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,63 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap} from "@angular/router";
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  // redScore: string = "0";
+  // blueScore: string = "0";
+  constructor(private route: ActivatedRoute,
+              @Inject('LOCALSTORAGE') private localStorage: any) {
+      const params = this.route.snapshot.params;
+      let num: number = parseInt(params['num']) || 1;
+      const data = route.snapshot.data;
+      const action: string = data.action || "";
+      switch(action) {
+        case "reset":
+          this.localStorage.clear();
+          this.localStorage.setItem('red', 0);
+          this.localStorage.setItem('blue', 0);
+          break;
+        case "incrementBlue":
+          this.localStorage.setItem('blue', parseInt(this.localStorage.getItem('blue')) + num);
+          break;
+        case "incrementRed":
+          this.localStorage.setItem('red', parseInt(this.localStorage.getItem('red')) + num);
+          break;
+        case "setRed":
+          this.localStorage.setItem('red', num);
+          break;
+        case "setBlue":
+          this.localStorage.setItem('blue', num);
+          break;
+        default:
+          break;
+      }
+    this.route.queryParams.subscribe(params => {
+      if (params['red']) {
+        this.localStorage.setItem('red', parseInt(params['red']));
+      }
+      if (params['blue']) {
+        this.localStorage.setItem('blue', parseInt(params['blue']));
+      }
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+  get blueScore() {
+    return this.localStorage.getItem('blue') || "0";
+  }
+
+  get redScore() {
+    return this.localStorage.getItem('red') || "0";
+
+  }
+
+}
